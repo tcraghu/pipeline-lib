9300281ea9a65310d969dbb4aa3f9f4cd6f3e480
Raghu <tcraghu@yahoo.com>
2022-02-23 11:20:50 -0800

    update

--- vars/buildstep.groovy
+++ vars/buildstep.groovy
@@ -45,11 +45,13 @@ def call(body) {
                 agent {
                     
 
+                        docker {
+                                image 'maven:3.8.1-adoptopenjdk-11'
+                                label 'my-defined-label'
+                                args  '-v /tmp:/tmp'
+                            }
+                                        
 
-                               dockerfile {
-                                   filename 'DockerFile'
-                                    
-                                }
                 }
 
 
